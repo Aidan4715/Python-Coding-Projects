aab051ed9e3ec9ae2c4a501164ea13b0497dbfc3
Aidan4715 <Aidanjoshi@csu.fullerton.edu>
2025-10-14 16:36:19 -0700

    made it more efficient

--- Python-Math-Calculator/mathematics/numbers/series.py
+++ Python-Math-Calculator/mathematics/numbers/series.py
@@ -1,13 +1,9 @@
 def sum(my_list):
     total = 0
     for i in my_list:
-        total = total + my_list[i]
+        total = total + i
     return total
 
 def average(my_list):
-    total = 0
-    count = 0
-    for i in my_list:
-        total = total + my_list[i]
-        count = count + 1
-    return total/count
+    average = sum(my_list)/len(my_list)
+    return average
