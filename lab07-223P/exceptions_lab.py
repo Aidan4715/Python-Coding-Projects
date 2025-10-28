59e6562f346d203696a0ad1d72f0c1070f7818cb
Aidan4715 <Aidanjoshi@csu.fullerton.edu>
2025-10-28 16:36:01 -0700

    did custom exception function with prime not found error class

--- lab07-223P/exceptions_lab.py
+++ lab07-223P/exceptions_lab.py
@@ -1,3 +1,7 @@
+class PrimeNotFoundError(Exception):
+    pass
+
+
 def divide_numbers(numerator, denominator):
     try:
         number = numerator/denominator
@@ -13,3 +17,12 @@ def parse_int(value):
         print("Error: Invalid integer")
     else:
         return number
+
+def custom_exception(value):
+    loop = value - 1
+    for (i+2) in range(loop):
+        if(value%i = 0):
+            raise PrimeNotFoundError("Given number is not prime number")
+    return value
+
+    
