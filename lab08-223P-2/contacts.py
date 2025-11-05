e205bccd15cabc5b4b99c02fef4eb97535d1a35f
Aidan4715 <Aidanjoshi@csu.fullerton.edu>
2025-11-04 16:16:14 -0800

    Currently doing add_contact function have done the adding contact part currently wokring on sorting it in the contacts dictionary

--- lab08-223P-2/contacts.py
+++ lab08-223P-2/contacts.py
@@ -13,4 +13,17 @@ class Contacts:
         except FileNotFoundError:
             print(f"File  '{self.file}' does not exist or cannot be found. Restarting with an empty contact list.")
             self.dict = {}
+    
+    def add_contact(self, phone, first_name, last_name):
+
+        if phone in self.dict:
+            print("error")
+
+        self.dict[phone] = [first_name, last_name]
+
+        for names in self.dict.values():
+            for names in self.dict.values():
+                if ()
+
+
 
